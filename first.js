2ef44d54550c6f1a75ecbd2a84981d289ff26901
Aayush <as3122005@gmail.com>
2025-01-31 14:22:58 +0530

    m

---
+++ first.js
@@ -0,0 +1,21 @@
+const myBoolean=true;
+const myString='hello world';
+const firstNumber=20;
+let secondNumber=40;
+secondNumber=80;
+console.log(secondNumber)
+const myArray=[myBoolean,myString]
+console.log(myArray)
+const myObject={firstProperty:myArray,sumProperty:firstNumber+secondNumber}
+console.log(myObject)
+console.log(myObject.sumProperty)
+console.log(myObject.firstProperty[1 ])
+const objectVariable={
+    property1:'i am property 1',
+    property2:'i am property 2',
+    property3:[20,30,40]
+};
+ console.log(objectVariable.property3[2]);
+ const myArray2=[20,30,40];
+ console.log(myArray2[2]);
+ 
